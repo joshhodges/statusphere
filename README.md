75d0f89f25966c4152bb3731aafe2b5372f88863
Josh Hodges <jh.hodges28@gmail.com>
2015-10-11 11:48:46 -0400

    Weather finished

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# statusphere
+# Statusphere
 Meteor Global Distributed Hackathon 2015
+
+#Packages
+1. Iron Router
+2. http
+3. chfritz:easycron
--- statusphere.js
+++ statusphere.js
@@ -1,23 +1,77 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+	Meteor.call("Weather");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  
+  Router.route('/', function () {
+	this.render('home');
+	});
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    var everyMinute = new Cron(function() {
+	    Meteor.call("Weather");
+        console.log("another minute has passed!");
+    }, {});
+    
+  });
+  
+  Meteor.methods({
+	Weather: function () {
+	var temp = 0
+		Meteor.call("getTemperature", function(error, results) {
+		       temp = JSON.parse(results.content).list[0].temp.day;
+		});
+		console.log(temp);
+		Meteor.call("changeForecast", getTempColor(temp));
+		
+	},
+	
+	getTemperature: function () {
+	var city = Meteor.settings.city;
+  		this.unblock();
+  		try {
+    		var result = HTTP.call("GET", "http://api.openweathermap.org/data/2.5/forecast/daily",
+                           {params: {
+	                           q: city + ',us',
+	                           APPID: Meteor.settings.openWeatherAPIkey,
+	                           units: 'imperial'
+	                           }});
+			return result;
+  		} catch (e) {
+			return false;
+  		}
+	},
+	
+	changeForecast: function (color) {
+  		this.unblock();
+  		try {
+    		var result = HTTP.call("POST", "https://api.particle.io/v1/devices/"+Meteor.settings.particleDeviceID+"/setColor",
+                           {params: {
+	                           arg: color,
+	                           access_token: Meteor.settings.particleAccessToken
+	                           }});
+			return true;
+  		} catch (e) {
+			return false;
+  		}
+	}
   });
 }
+
+function getTempColor(temp){
+	red = 255;
+	blue = 255;
+	
+	
+	temp = Math.round(temp);
+	if(temp > 50){
+		blue -= (temp - 50) * 5;
+	}
+	else if(temp < 50){
+		blue -= (50 - temp) * 5; 
+	}
+	
+	return red +',0,'+blue;
+}
+
